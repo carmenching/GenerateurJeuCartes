232c6004ae85be46fd7fea7262cd69e85e2c6b0a
meowmeowky <meowmeowky517@gmail.com>
2020-10-31 13:45:13 +0100

    added all comments

--- generer.py
+++ generer.py
@@ -9,51 +9,76 @@ class Carte:
 
     Attributs:
     ----------
-    suite : str
+    description : str
         description simple d'une carte
+    valeur : int
+        valeur en entier d'une carte
 
     '''
     def __init__(self, description, valeur):
+        """ Constructeur d'objet Carte
+        Parameters:
+        ----------
+        description : str
+            description simple d'une carte
+        valeur : int
+            valeur en entier d'une carte
+        """
         self.description = description
         self.valeur = valeur
 
     def afficher(self):
+        """Afficher la valeur et la description de la carte"""
         return "{} de {}".format(self.valeur, self.description)
 
 class Paquet:    
     '''
-    une classe répresentant d'un Paquet - un ensemble de carte
+    Une classe répresentant d'un Paquet - un ensemble de carte
     ...
 
     Attributs:
-
+    ----------
+    _cartes : list
+        une liste d'objet Carte qui constitue le paquet
     '''
-    # def __init__(self, cartes=[]):
-    #     self.set_cartes(cartes)
+
     def __init__(self, nbrCartes, listeValeursCartes):
+        """ Constructeur d'objet Paquet
+        Parameters:
+        ----------
+        nbrCartes : int
+            nombre de cartes à créer pour le paquet
+        listeValeursCartes : int
+            une liste des variantes de descriptions pour chaque nbrCartes
+        """
         self.construire(nbrCartes, listeValeursCartes)
 
     def get_cartes(self):
+        """Getter défaut d'attribut protégé 'cartes'"""
         return self._cartes
 
     def set_cartes(self, cartes):
+        """Setter défaut d'attribut protégé 'cartes'"""
         self._cartes = cartes
     
     # construire un paquet de cartes personnalisé
-    def construire(self, nombre, liste_valeurs):
+    def construire(self, nbrCartes, liste_valeurs):
+        """construire un paquet de cartes personnalisé - méthode appelé par défaut par constructeur
+        Parameters:
+        -----------
+        nbrCartes : int
+            nombre de cartes à créer pour le paquet
+        listeValeursCartes : int
+            une liste des variantes de descriptions pour chaque nbrCartes
+        """
         self._cartes = []
         for s in liste_valeurs:
-            for valeur in range(1, nombre):
+            for valeur in range(1, nbrCartes):
                 self._cartes.append(Carte(s, valeur))
 
-    # construire un paquet de cartes personnalisé
-    # def construire(self, nombre, liste_valeurs):
-    #     for s in liste_valeurs:
-    #         for valeur in range(1, nombre):
-    #             self._cartes.append(Carte(s, valeur))
-
-    # mélanger le paquet de cartes
+    # 
     def melanger_cartes(self):
+        """mélanger le paquet de cartes"""
         tmp = self.get_cartes()
         paquet = []
         while len(tmp) != 0:
@@ -62,8 +87,14 @@ class Paquet:
             tmp.remove(carte)
         self._cartes = paquet
 
-    # distribuer le paquet de cartes au 'param - joueurs
+    # 
     def distribuer_cartes(self, joueurs):
+        """distribuer le paquet de cartes au 'param - joueurs
+        Parameters:
+        -----------
+        joueurs : list
+            une liste d'objets Joueur
+        """
         tmp = self.get_cartes()
         cartes = {}
         while len(tmp) != 0:
@@ -74,13 +105,20 @@ class Paquet:
                 joueur.ajouter_fin_main(carte)
                 tmp.remove(carte)
 
-    # enlever la liste de cartes du paquet
+    # 
     def enlever_cartes(self, cartes):
+        """Enlever la liste de cartes du paquet
+        Parameters:
+        -----------
+        cartes : list
+            une liste d'objets Carte
+        """
         for carte in cartes:
             self._cartes.remove(carte)
 
-    # afficher la carte du paquet
+    # 
     def afficher(self):
+        """afficher la carte du paquet"""
         for c in self._cartes:
             print(c.afficher())
 
---
+++ readme.md
@@ -0,0 +1,13 @@
+
+Instructions pour le début d'utilisation: 
+    * Un ensemble de méthodes est prédéfini dans la classe principale pour la création d'un jeu de combat fermé simple - à des fins de test
+
+Arborescene d'application: 
+-JeuxCartes
+    |
+    |---config.py : Les classes pour la configuration et la déroulement d'un jeu
+    |
+    |---generer.py : Les classes pour la génération des objets utilisé dans les jeux
+    |
+    |---main.py : La méthode main d'application
+
